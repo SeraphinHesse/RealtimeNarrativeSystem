a9275e4d27b17ec43dd8555ea6f7dbef2616f9fd
Pikkson <166311178+Pikkson@users.noreply.github.com>
2024-04-13 19:19:20 +0200

    New Faction Class TESTING

---
+++ RTNSUNITY2/Assets/Faction Scripts/FactionParent - Copy.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FactionParent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    static public void main(string[] args){
+        int Wealth = 0;
+        Debug.Log(Wealth);
+    }
+    
+    void Start()
+    {
+    
+    
+    /*
+    int Manpower, ManpowerInf;
+    int Equipment EquipmentInf;
+    int Influence, Intelligence, Cohesion;
+    */
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
