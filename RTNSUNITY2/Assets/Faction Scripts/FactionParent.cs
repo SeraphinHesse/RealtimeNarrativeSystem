71826b4ba921dec99f6dd5373fd4c18eb0e36c20
Pikkson <166311178+Pikkson@users.noreply.github.com>
2024-04-21 13:55:21 +0200

    Changed Stats to Arrays

--- RTNSUNITY2/Assets/Faction Scripts/FactionParent.cs
+++ RTNSUNITY2/Assets/Faction Scripts/FactionParent.cs
@@ -8,7 +8,13 @@ public class FactionParent : MonoBehaviour
     public TextMeshProUGUI Wealthtxt;
     public TextMeshProUGUI WealthInftxt;
 
-
+// Wealth = HardStats[0] | Manpower = Hardstats[1] | Equipment = HardStats[2]
+    int[] HardStats = {5, 1, 2};
+// WealthInf = Infrastucture[0] | Manpower = Infrastructure[1] | Infrastructure = HardStats[2]
+    int[] Infrastucture = {2, 0, 0};
+// Influence = SoftStats[0] | Intelligence = SoftStats[1] | Cohesion = Softstats[2]
+    int[] Softstats = {2, 0, 0};
+/*
     int Wealth = 5;
     int WealthInf = 2;
     int Manpower = 0;
@@ -18,12 +24,19 @@ public class FactionParent : MonoBehaviour
     int Influence = 0;
     int Intelligence = 0;
     int Cohesion = 0;
+*/
 
     // Start is called before the first frame update
     void Start(){
+        
         Cycle.OnCycleUpdate += UpdateStats;
-        Wealthtxt.text = Wealth.ToString();
-        WealthInftxt.text = WealthInf.ToString();
+        Wealthtxt.text = HardStats[0].ToString();
+        WealthInftxt.text = Infrastucture[0].ToString();
+        for (int i = 0; i < HardStats.Length ; i++){
+        Debug.Log(HardStats[i]);
+        }
+    
+        
     }
 
     void FactionCycleUpdate()
@@ -36,8 +49,8 @@ public class FactionParent : MonoBehaviour
     void UpdateStats()
     {
         Debug.Log("it Worked!!");
-        Wealth = Wealth + (WealthInf / 2);
-        Wealthtxt.text = Wealth.ToString();
+        HardStats[0] = HardStats[0] + (Infrastucture[0] / 2);
+        Wealthtxt.text = HardStats[0].ToString();
             ;
 
       
